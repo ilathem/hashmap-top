1ae1a6a93ad8f7d252d869278d5eebec3783ba3f
ilathem <isaiahlathem@gmail.com>
2024-05-17 18:07:45 -0500

    Add get(key)

--- script.js
+++ script.js
@@ -5,65 +5,65 @@ class LinkedList {
   }
 
   toString() {
-    let string = ` ( ${this.root?.value ?? "null"} )`;
+    let string = `[ ${this.root?.key}, ${this.root?.value} ]`;
     let node = this.root;
     while (node) {
-      string += ` -> ( ${node.next?.value ?? "null"} )`; // optional chaining!
+      string += `, [ ${this.root?.key}, ${this.root?.value} ]`; 
       node = node.next;
     }
     console.log(string);
   }
 
-  append(value) {
+  append(key, value) {
     if (this.root === null) {
-      this.root = new Node(value);
+      this.root = new Node(key, value);
     } else {
       let node = this.root;
       while (node) {
         if (node.next) node = node.next;
         else {
-          node.next = new Node(value);
+          node.next = new Node(key, value);
           return;
         }
       }
     }
   }
 
-  prepend(value) {
-    if (this.root === null) {
-      this.root = new Node(value);
-    } else {
-      let node = this.root;
-      this.root = new Node(value);
-      this.root.next = node;
-    }
-  }
+  // prepend(value) {
+  //   if (this.root === null) {
+  //     this.root = new Node(value);
+  //   } else {
+  //     let node = this.root;
+  //     this.root = new Node(value);
+  //     this.root.next = node;
+  //   }
+  // }
 
-  size() {
-    if (this.root === null) return 0;
-    else {
-      let size = 0;
-      let node = this.root;
-      while (node) {
-        size++;
-        node = node.next;
-      }
-      return size;
-    }
-  }
+  // size() {
+  //   if (this.root === null) return 0;
+  //   else {
+  //     let size = 0;
+  //     let node = this.root;
+  //     while (node) {
+  //       size++;
+  //       node = node.next;
+  //     }
+  //     return size;
+  //   }
+  // }
 
   head() {
     return this.root;
   }
 
-  tail() {
-    let node = this.root;
-    if (!node) return null;
-    while (node.next) {
-      node = node.next;
-    }
-    return node;
-  }
+  // tail() {
+  //   let node = this.root;
+  //   if (!node) return null;
+  //   while (node.next) {
+  //     node = node.next;
+  //   }
+  //   return node;
+  // }
 
   at(index) {
     if (index < 0) return null;
@@ -80,39 +80,39 @@ class LinkedList {
   }
 
   // remove last element and returns it
-  pop() {
-    if (!this.root) return null;
-    if (this.size() === 1) {
-      const node = this.root;
-      this.root = null;
-      return node;
-    }
-    let node = this.root;
-    while (node.next.next) {
-      node = node.next;
-    }
-    const nodeToPop = node.next;
-    node.next = null;
-    return nodeToPop;
-  }
+  // pop() {
+  //   if (!this.root) return null;
+  //   if (this.size() === 1) {
+  //     const node = this.root;
+  //     this.root = null;
+  //     return node;
+  //   }
+  //   let node = this.root;
+  //   while (node.next.next) {
+  //     node = node.next;
+  //   }
+  //   const nodeToPop = node.next;
+  //   node.next = null;
+  //   return nodeToPop;
+  // }
 
   // return true if value in list, otherwise return false
-  contains(value) {
-    if (!this.root) return false;
-    let node = this.root;
-    while (node) {
-      if (node.value === value) return true;
-      node = node.next;
-    }
-  }
+  // contains(value) {
+  //   if (!this.root) return false;
+  //   let node = this.root;
+  //   while (node) {
+  //     if (node.value === value) return true;
+  //     node = node.next;
+  //   }
+  // }
 
-  // returns the index of the node containing value, or null if not found
-  find(value) {
+  // returns the node of the node containing key, or null if not found
+  find(key) {
     if (!this.root) return false;
     let node = this.root;
     let i = 0;
     while (node) {
-      if (node.value === value) return i;
+      if (node.key === key) return node;
       i++;
       node = node.next;
     }
@@ -121,69 +121,70 @@ class LinkedList {
 
   // insertAt(value, index) that inserts a new node with the
   // provided value at the given index.
-  insertAt(value, index) {
-    if (index < 0) {
-      console.error("Cannot insert at negative indices");
-      return false;
-    }
-    if (index === 0) {
-      this.prepend(value);
-      return true;
-    }
-    let i = 1;
-    let node = this.root;
-    while (node.next) {
-      if (index === i) {
-        const oldNode = node.next;
-        const newNode = new Node(value);
-        node.next = newNode;
-        newNode.next = oldNode;
-        return true;
-      }
-      node = node.next;
-      i++;
-    }
-    if (index === i) {
-      this.append(value);
-      return true;
-    } else {
-      console.error("Cannot insert past list bounds");
-      return false;
-    }
-  }
+  // insertAt(value, index) {
+  //   if (index < 0) {
+  //     console.error('Cannot insert at negative indices');
+  //     return false;
+  //   }
+  //   if (index === 0) {
+  //     this.prepend(value);
+  //     return true;
+  //   }
+  //   let i = 1;
+  //   let node = this.root;
+  //   while (node.next) {
+  //     if (index === i) {
+  //       const oldNode = node.next;
+  //       const newNode = new Node(value);
+  //       node.next = newNode;
+  //       newNode.next = oldNode;
+  //       return true;
+  //     }
+  //     node = node.next;
+  //     i++;
+  //   }
+  //   if (index === i) {
+  //     this.append(value);
+  //     return true;
+  //   } else {
+  //     console.error('Cannot insert past list bounds');
+  //     return false;
+  //   }
+  // }
 
-  removeAt(index) {
-    if (index < 0) {
-      console.error("Cannot remove at negative indices");
-      return null;
-    }
-    if (index === 0) {
-      if (!this.root) {
-        console.error('Cannot remove that which is empty');
-        return null;
-      }
-      const removedNode = this.root;
-      this.root = this.root.next;
-      return removedNode;
-    }
-    let i = 1;
-    let node = this.root;
-    while (node.next) {
-      if (index === i) {
-        const removedNode = node.next;
-        node.next = removedNode.next;
-        return removedNode;
-      }
-      node = node.next;
-      i++;
-    }
-    console.error("Cannot remove past list bounds");
-    return null;
-  }
+  // removeAt(index) {
+  //   if (index < 0) {
+  //     console.error('Cannot remove at negative indices');
+  //     return null;
+  //   }
+  //   if (index === 0) {
+  //     if (!this.root) {
+  //       console.error('Cannot remove that which is empty');
+  //       return null;
+  //     }
+  //     const removedNode = this.root;
+  //     this.root = this.root.next;
+  //     return removedNode;
+  //   }
+  //   let i = 1;
+  //   let node = this.root;
+  //   while (node.next) {
+  //     if (index === i) {
+  //       const removedNode = node.next;
+  //       node.next = removedNode.next;
+  //       return removedNode;
+  //     }
+  //     node = node.next;
+  //     i++;
+  //   }
+  //   console.error('Cannot remove past list bounds');
+  //   return null;
+  // }
 }
 
 class Node {
-  constructor(value) {
+  constructor(key, value) {
+    this.key = key;
     this.value = value;
     this.next = null;
   }
@@ -206,17 +207,28 @@ class HashMap {
     return hashCode;
   }
 
+  // update key with value, create if non-existent
   set(key, value) {
     const bucket = this.map[this.hash(key)];
     if (bucket.head()) {
-      const index = bucket.find(value);
-      if (index) bucket.at(index).value = value;
-      else bucket.append(value);
+      const node = bucket.find(value);
+      if (node) node.value = value;
+      else bucket.append(key, value);
     } else {
-      bucket.append(value);
+      bucket.append(key, value);
     }
+  }
+
+  // return value associated with key, or null if none
+  get(key) {
+    const bucket = this.map[this.hash(key)];
+    console.log(this.map[this.hash(key)])
+    const node = bucket.find(key);
+    if (node) return node.value;
+    return null;
   }
 }
 
 const map = new HashMap();
-console.log(map.hash('Carlos'));
+map.set('Carlos', 'A cool guy');
+console.log(map.get('Carlos'))
